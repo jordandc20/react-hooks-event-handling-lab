9a4d397eda3118d2f3cc9780c1e8b52086971ef5
Diana Jordan <dianacjordan@gmail.com>
2023-03-17 17:55:50 -0400

    comnplete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe (){
+
+function handleFocus(e){
+    e.type==='focus'? console.log('Good!') : console.log("Hey! Eyes on me!")
+}
+
+
+    return (
+        <div>
+           <button onFocus={handleFocus} onBlur={handleFocus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;// Code EyesOnMe Component Here
